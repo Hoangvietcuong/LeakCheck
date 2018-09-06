344d167db2cbd8542a58603e4a57ee2b77585757
root <vietcuongd13dt3@gmail.com>
2018-09-06 15:49:42 +0700

    add update file

---
+++ update.sh
@@ -0,0 +1,6 @@
+#sudo -S git clone https://github.com/HoangVietCuong/LeakCheck.git
+cd /usr/bin/LeakCheck/
+pwd
+sudo -S git pull 
+sudo -S rm -rf /usr/bin/LeakCheck_V1.2
+sudo -S cp LeakCheck_V1.2 /usr/bin/
