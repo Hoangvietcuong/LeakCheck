0b606506130e8d7b8cd0cf9def455b3d9058154d
root <vietcuongd13dt3@gmail.com>
2018-09-06 16:30:30 +0700

    add desktop application file

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 #sudo -S git clone https://github.com/HoangVietCuong/LeakCheck.git
 cd /usr/bin/LeakCheck/
-pwd
-sudo -S git pull 
+sudo -S git pull  origin maste
 sudo -S rm -rf /usr/bin/LeakCheck_V1.2
+sudo -S cp update.sh /usr/bin/
 sudo -S cp LeakCheck_V1.2 /usr/bin/
