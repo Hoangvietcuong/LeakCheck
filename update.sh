0775edac1dbc474787fd9721d7bb05675e846d64
root <vietcuongd13dt3@gmail.com>
2018-09-07 09:14:39 +0700

    fix bug ouldn't find remote ref maste

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #sudo -S git clone https://github.com/HoangVietCuong/LeakCheck.git
 cd /usr/bin/LeakCheck/
-sudo -S git pull  origin maste
-#sudo -S rm -rf /usr/bin/LeakCheck_V1.2
-#sudo -S cp update.sh /usr/bin/
-#sudo -S cp LeakCheck_V1.2 /usr/bin/
-#sudo -S cp update.desktop /home/pi/Desktop/Leak-Check/
+sudo -S git pull
+sudo -S rm -rf /usr/bin/LeakCheck_V1.2
+sudo -S cp update.sh /usr/bin/
+sudo -S cp LeakCheck_V1.2 /usr/bin/
+sudo -S cp update.desktop /home/pi/Desktop/Leak-Check/
